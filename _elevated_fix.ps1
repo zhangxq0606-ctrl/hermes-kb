9739aaca6066ab6a66f319e79c41f54733263d52
Zhangxq0606 <Zhangxq0606@gmail.com>
2026-06-13 23:18:02 +0800

    auto: sync 2026-06-13 23:18

--- _elevated_fix.ps1
+++
@@ -1,5 +0,0 @@
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-WindowStyle Hidden -NoProfile -File D:\hermes-kb\auto_sync.ps1" -WorkingDirectory "D:\hermes-kb"
-Set-ScheduledTask -TaskName HermesAutoSync -Action $action
-schtasks /delete /tn HermesElevatedFix /f > $null 2>&1
-Remove-Item "D:\hermes-kb\_elevated_fix.ps1" -Force
-Remove-Item "D:\hermes-kb\_fix_hidden.bat" -Force
--- _fix_task.ps1
+++
@@ -1,4 +0,0 @@
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-WindowStyle Hidden -NoProfile -File D:\hermes-kb\auto_sync.ps1" -WorkingDirectory "D:\hermes-kb"
-Set-ScheduledTask -TaskName HermesAutoSync -Action $action
-Write-Host "Done"
-Remove-Item -Path "D:\hermes-kb\_fix_task.ps1" -Force
